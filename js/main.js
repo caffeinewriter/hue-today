1c73c77e62dcf9a42230bfe9b9e1ef2b37b1531f
Brandon Anzaldi <brandon@brandonanzaldi.com>
2018-07-16 04:15:43 -0700

    Add YIQ calculation and alpha compensation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
+function computeYIQ() {
+	let bgColor = document.querySelector("body").style.background;
+	let rgbRegex = /rgba?\((\d+), ?(\d+), ?(\d+)(?:, ([0-9\.]+))?\)/gi;
+	let matches = rgbRegex.exec(bgColor);
+	if (matches && matches.length >= 4) {
+		let r = matches[1];
+		let g = matches[2];
+		let b = matches[3];
+		let a = !Number.isNaN(parseFloat(matches[4])) ? parseFloat(matches[4]) : 1;
+		let yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+		return yiq >= 128 || a < 0.5 ? "black" : "white";
+	}
+	return "black";
+}
+
 function updateColor() {
-  console.log("Update");
+  console.log("Update color");
   let colorInput = document.querySelector("#color");
   document.querySelector("body").style.background = colorInput.value;
+  let yiqRes = computeYIQ();
+  document.querySelector("#color").style.color = yiqRes;
+  document.querySelector("#color").style.borderBottomColor = yiqRes;
 }
 
 function updateHash() {
